716cbf73188dcc9cfd26b129e3f4708127cf3cd7
George Zalepa <george.zalepa@gmail.com>
2017-07-31 07:41:48 -0400

    Link tickets and users upon ticket creation

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,6 +1,7 @@
 # Ticket Model
 class Ticket < ApplicationRecord
   belongs_to :project
+  belongs_to :author, class_name: 'User'
   validates :name, presence: true
   validates :description, presence: true, length: { minimum: 10 }
 end
