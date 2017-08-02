5a4c5665fb30c2afc6e648b231839e3577a08949
George Zalepa <george.zalepa@gmail.com>
2017-08-02 08:03:46 -0400

    Archived users cannot sign in to the app

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,12 +5,20 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 
   scope :excluding_archived, lambda { where(archived_at: nil) }
-  
+
   def to_s
     "#{email} (#{admin? ? 'Admin' : 'User'})"
   end
 
   def archive
     self.update(archived_at: Time.now)
+  end
+
+  def active_for_authentication?
+    super && archived_at.nil?
+  end
+
+  def inactive_message
+    archived_at.nil? ? super : :archived
   end
 end
